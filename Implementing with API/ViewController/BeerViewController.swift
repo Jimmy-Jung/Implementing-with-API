8415b1c9704dbcbc65634f637b2f2d6270c93cbb
Jimmy <joony300@gmail.com>
2023-08-08 16:55:12 +0900

    [Add] ATS Allow

---
+++ Implementing with API/ViewController/BeerViewController.swift
@@ -0,0 +1,29 @@
+//
+//  BeerViewController.swift
+//  Implementing with API
+//
+//  Created by 정준영 on 2023/08/08.
+//
+
+import UIKit
+
+class BeerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
